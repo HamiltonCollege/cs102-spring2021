d5a0d56b6a969377f45e4c8c77398479cd0ba975
Dave-Perkins <42650290+Dave-Perkins@users.noreply.github.com>
2021-01-29 14:57:42 -0500

    Create height.cpp
    An exercise for Lab 01

---
+++ labs/lab01/for_you/height.cpp
@@ -0,0 +1,22 @@
+/**
+  CS-102 Lab 01: Given feet and inches, converts to total inches.
+  @file height.cpp
+  @author
+  @date
+*/
+
+#include <iostream>
+
+int get_inches(int feet, int inches) {
+  return 0; // replace this line
+}
+
+int main() {
+  std::cout << "Feet: ";
+  int feet;
+  std::cin >> feet;
+
+  // add code here
+
+  return 0;
+}
