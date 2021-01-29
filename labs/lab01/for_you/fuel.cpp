66ca5a3d8143e8d58825d125e1493d1d4dbcd664
Dave-Perkins <42650290+Dave-Perkins@users.noreply.github.com>
2021-01-29 15:32:02 -0500

    Create fuel.cpp

---
+++ labs/lab01/for_you/fuel.cpp
@@ -0,0 +1,11 @@
+/**
+  CS-102 Lab 01: Given an amount of fuel and a distance traveled,
+                 calculates the average fuel efficiency.
+  @file fuel.cpp
+  @author
+  @date
+*/
+
+int main() {
+  // it's all up to you now :)
+}
