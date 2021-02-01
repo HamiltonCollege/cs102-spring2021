ac19817d481751f1a7d80e258145cd8ce7ec3610
Darren Strash <darren.strash@gmail.com>
2021-01-31 19:45:13 -0500

    Comment using doxygen style

--- labs/lab01/for_you/height.cpp
+++ labs/lab01/for_you/height.cpp
@@ -1,22 +1,28 @@
 /**
-  CS-102 Lab 01: Given feet and inches, converts to total inches.
-  @file height.cpp
-  @author
-  @date
-*/
-
+ * CS-102 Lab 01: Given feet and inches, converts to total inches.
+ * @file height.cpp
+ * @author
+ * @collaborators
+ * @date
+ */
 #include <iostream>
 
-int get_inches(int feet, int inches) {
-  return 0; // replace this line
+/**
+ *
+ * @param feet
+ * @return
+ */
+int get_inches(int feet) {
+    return 0; // replace this line
 }
 
+// Controls operation of the program.
 int main() {
-  std::cout << "Feet: ";
-  int feet;
-  std::cin >> feet;
+    std::cout << "Feet: ";
+    int feet = 0;
+    std::cin >> feet;
 
-  // add code here
+    // add code here
 
-  return 0;
+    return 0;
 }
