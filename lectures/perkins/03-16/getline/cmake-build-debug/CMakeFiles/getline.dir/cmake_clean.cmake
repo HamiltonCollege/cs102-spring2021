39abe7aa2900814115a1045d665b4e8291acfadc
Dave-Perkins <dperkins@hamilton.edu>
2021-03-16 09:51:27 -0400

    Tuesday March 16 lecture code

---
+++ lectures/perkins/03-16/getline/cmake-build-debug/CMakeFiles/getline.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/getline.dir/main.cpp.o"
+  "CMakeFiles/getline.dir/old.cpp.o"
+  "getline"
+  "getline.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/getline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
